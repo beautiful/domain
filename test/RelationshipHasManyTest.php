6b3a956774db14c72aaa4649c4843f1bc5797773
Luke Morton <lukemorton.designs@gmail.com>
2012-01-04 10:07:53 +0000

    Add has many tests.

---
+++ test/RelationshipHasManyTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Test a has one relationship between domains
+ * 
+ * @package     Beautiful
+ * @subpackage  Beautiful Domain
+ * @category    Relationship
+ * @category    Test
+ * @author      Luke Morton
+ * @copyright   Luke Morton, 2011
+ * @license     MIT
+ */
+class RelationshipHasManyTest extends RelationshipTest {
+
+	protected static $class = 'Relationship_HasMany';
+
+}
